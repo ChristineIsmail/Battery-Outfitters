881f8f8622a7379a816e76c7464ef59bef4848e7
ChristineIsmail <christineismail19@gmail.com>
2023-01-06 10:20:24 +0200

    logo+removing indicators+name of the app

--- Frontend/src/pages/HomePage/HomePage.tsx
+++ Frontend/src/pages/HomePage/HomePage.tsx
@@ -35,7 +35,114 @@ function HomePage() {
               alt="Second slide"
             />
           </Carousel.Item>
-
+          <Carousel.Item>
+            <img
+              className=" second-slide-2 "
+              src="i10.jpg"
+              alt="Second slide"
+            />
+            <img
+              className=" second-slide-1 "
+              src="i10 cropped.jpg"
+              alt="Second slide"
+            />
+          </Carousel.Item>{" "}
+          <Carousel.Item>
+            <img
+              className="first-slide-2 "
+              src="truckk.jpg"
+              alt="First slide"
+            />
+            <img
+              className="first-slide-1 "
+              src="pickup2.jpg"
+              alt="First slide"
+            />
+          </Carousel.Item>
+          <Carousel.Item>
+            <img
+              className=" second-slide-2 "
+              src="ford.jpeg"
+              alt="Second slide"
+            />
+            <img
+              className=" second-slide-1 "
+              src="ford2.jpg"
+              alt="Second slide"
+            />
+          </Carousel.Item>
+          <Carousel.Item>
+            <img
+              className=" second-slide-2 "
+              src="i10.jpg"
+              alt="Second slide"
+            />
+            <img
+              className=" second-slide-1 "
+              src="i10 cropped.jpg"
+              alt="Second slide"
+            />
+          </Carousel.Item>{" "}
+          <Carousel.Item>
+            <img
+              className="first-slide-2 "
+              src="truckk.jpg"
+              alt="First slide"
+            />
+            <img
+              className="first-slide-1 "
+              src="pickup2.jpg"
+              alt="First slide"
+            />
+          </Carousel.Item>
+          <Carousel.Item>
+            <img
+              className=" second-slide-2 "
+              src="ford.jpeg"
+              alt="Second slide"
+            />
+            <img
+              className=" second-slide-1 "
+              src="ford2.jpg"
+              alt="Second slide"
+            />
+          </Carousel.Item>
+          <Carousel.Item>
+            <img
+              className=" second-slide-2 "
+              src="i10.jpg"
+              alt="Second slide"
+            />
+            <img
+              className=" second-slide-1 "
+              src="i10 cropped.jpg"
+              alt="Second slide"
+            />
+          </Carousel.Item>{" "}
+          <Carousel.Item>
+            <img
+              className="first-slide-2 "
+              src="truckk.jpg"
+              alt="First slide"
+            />
+            <img
+              className="first-slide-1 "
+              src="pickup2.jpg"
+              alt="First slide"
+            />
+          </Carousel.Item>
+          <Carousel.Item>
+            <img
+              className=" second-slide-2 "
+              src="ford.jpeg"
+              alt="Second slide"
+            />
+            <img
+              className=" second-slide-1 "
+              src="ford2.jpg"
+              alt="Second slide"
+            />
+          </Carousel.Item>
           <Carousel.Item>
             <img
               className=" second-slide-2 "
